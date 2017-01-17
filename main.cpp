628cb1d46820282a45ad75f357da212974917ee1
Eryk Andrzejewski <erykandrzejewski2000@gmail.com>
2017-01-17 20:04:21 +0100

    Poprawka kodu

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-using namespace std;
-int main(){
-	cout<<"Wybory tuż, tuż, a ty nadal nie wiesz na kogo głosować?"<<endl;
-	static string kandydat;
-	switch(rand()%11){
-		case 0: kandydat="Janusza Korwina-Mikke"; break;
-		case 1: kandydat="Andrzeja Dudę"; break;
-		case 2: kandydat="Pawła Kukiza"; break;
-		case 3: kandydat="Bronisława Komorowskiego"; break;
-		case 4: kandydat="Magdalenę Ogórek"; break;
-		case 5: kandydat="Janusza Palikota"; break;
-		case 6: kandydat="Grzegorza Brauna"; break;
-		case 7: kandydat="Andrzeja Jarubasa"; break;
-		case 8: kandydat="Pawła Tanajno"; break;
-		case 9: kandydat="Mariana Kowalskiego"; break;
-		case 10: kandydat="Jacka Wilka"; break;
+
+int main()
+{
+	std::cout << "Wybory tuż, tuż, a ty nadal nie wiesz na kogo głosować?\n";
+	std::string yourCandidate;
+
+	switch(rand() % 11)
+	{
+		case 0: yourCandidate = "Janusza Korwina-Mikke"; break;
+		case 1: yourCandidate = "Andrzeja Dudę"; break;
+		case 2: yourCandidate = "Pawła Kukiza"; break;
+		case 3: yourCandidate = "Bronisława Komorowskiego"; break;
+		case 4: yourCandidate = "Magdalenę Ogórek"; break;
+		case 5: yourCandidate = "Janusza Palikota"; break;
+		case 6: yourCandidate = "Grzegorza Brauna"; break;
+		case 7: yourCandidate = "Andrzeja Jarubasa"; break;
+		case 8: yourCandidate = "Pawła Tanajno"; break;
+		case 9: yourCandidate = "Mariana Kowalskiego"; break;
+		case 10: yourCandidate = "Jacka Wilka"; break;
 	}
-	cout<<"Głosuj na "<<kandydat<<"! To kandydat twoich marzen!";
-	cin.get();
+	
+	yourCandidate = "Eryka Andrzejewskiego"; //Troll ;)
+
+	std::cout << "Głosuj na " << yourCandidate << "! To kandydat twoich marzeń!\n";
 } 
